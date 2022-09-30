dbdb0c6d4c93e3b191087f5ff472a305b8868495
Aryan0303 <110547212+Aryan0303@users.noreply.github.com>
2022-09-30 17:55:29 +0500

    project work in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
-// Import our custom CSS
-import '../scss/styles.scss'
 
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
 
-import Alert from 'bootstrap/js/dist/alert';
-
-import { Tooltip, Toast, Popover } from 'bootstrap';
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 
@@ -22,3 +15,36 @@ optionsList.forEach(o => {
     optionsContainer.classList.remove("active");
   });
 });
+/*first select box */
+const selected1 = document.querySelector(".selected1");
+const optionsContainer1 = document.querySelector(".options-container1");
+
+const optionsList1 = document.querySelectorAll(".option1");
+
+selected1.addEventListener("click", () => {
+  optionsContainer1.classList.toggle("active");
+});
+
+optionsList1.forEach(o => {
+  o.addEventListener("click", () => {
+    selected1.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer1.classList.remove("active");
+  });
+});
+/*second select box */
+const selected2 = document.querySelector(".selected2");
+const optionsContainer2 = document.querySelector(".options-container2");
+
+const optionsList2 = document.querySelectorAll(".option2");
+
+selected2.addEventListener("click", () => {
+  optionsContainer2.classList.toggle("active");
+});
+
+optionsList2.forEach(o => {
+  o.addEventListener("click", () => {
+    selected2.innerHTML = o.querySelector("label").innerHTML;
+    optionsContainer2.classList.remove("active");
+  });
+});
+/*third select box */
