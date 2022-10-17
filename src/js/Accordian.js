8e7cddbf1e087442b34232fd323bdcdfef42e3f5
Aryan0303 <110547212+Aryan0303@users.noreply.github.com>
2022-10-17 05:28:17 +0500

    all main changes

--- src/js/Accordian.js
+++ src/js/Accordian.js
@@ -1,18 +1,18 @@
 $(document).ready (function(){
     $("#accordion .card .card-link").click(function(){
-        if($(this).find("i.fa").hasClass("fa-plus"))
+        if($(this).find("i.bi").hasClass("bi-plus"))
         {
-            $(this).find("i.fa").removeClass("fa-plus");
-            $(this).find("i.fa").addClass("fa-minus");
+            $(this).find("i.bi").removeClass("bi-plus");
+            $(this).find("i.bi").addClass("bi-minus");
         }
-        else if($(this).find("i.fa").hasClass("fa-minus"))
+        else if($(this).find("i.bi").hasClass("bi-minus"))
         {
-            $(this).find("i.fa").removeClass("fa-minus");
-            $(this).find("i.fa").addClass("fa-plus");
+            $(this).find("i.bi").removeClass("bi-minus");
+            $(this).find("i.bi").addClass("bi-plus");
         }
 
-        $(this).parents(".card").siblings().find(".card-header .card-link i.fa").removeClass("fa-plus");
-        $(this).parents(".card").siblings().find(".card-header .card-link i.fa").removeClass("fa-minus");
+        $(this).parents(".card").siblings().find(".card-header .card-link i.bi").removeClass("bi-plus");
+        $(this).parents(".card").siblings().find(".card-header .card-link i.bi").removeClass("bi-minus");
 
     }); 
 });
